3663fa1c49f2e571ab46a4bd31266d59eb406895
cbignon <71795678+cbignon@users.noreply.github.com>
2020-12-04 12:43:23 +0100

    correction strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,18 +6,22 @@
 /*   By: cbignon <cbignon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/26 12:16:25 by cbignon           #+#    #+#             */
-/*   Updated: 2020/11/26 14:31:09 by cbignon          ###   ########.fr       */
+/*   Updated: 2020/12/04 12:40:37 by cbignon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stddef.h>
+#include "libft.h"
 
 char	*ft_strchr(const char *s, int c)
 {
 	size_t	x;
+	int		end;
 
 	x = 0;
-	while (s[x] || c == '\0')
+	end = ft_strlen((char *)s);
+	if (c == '\0')
+		return ((char *)s + end);
+	while (s[x])
 	{
 		if (s[x] == c)
 			return ((char *)s + x);
