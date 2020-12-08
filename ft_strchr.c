a1ec8830ac5b89ef4d62365858e80be451f62326
cbignon <71795678+cbignon@users.noreply.github.com>
2020-12-08 16:32:33 +0100

    strchr corrc norme

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: cbignon <cbignon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/26 12:16:25 by cbignon           #+#    #+#             */
-/*   Updated: 2020/12/04 13:36:38 by cbignon          ###   ########.fr       */
+/*   Updated: 2020/12/08 16:31:16 by cbignon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ char	*ft_strchr(const char *s, int c)
 	size_t	x;
 
 	x = 0;
-	if ( c == '\0')
+	if (c == '\0')
 		return ((char *)s + ft_strlen((char *)s));
 	while (s[x])
 	{
