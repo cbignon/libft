ce097fb09507907104941b1ec9ef41175fb8e9e3
cbignon <71795678+cbignon@users.noreply.github.com>
2020-12-16 12:42:34 +0100

    split again

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: cbignon <cbignon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/08 12:28:22 by cbignon           #+#    #+#             */
-/*   Updated: 2020/12/16 10:53:44 by cbignon          ###   ########.fr       */
+/*   Updated: 2020/12/16 12:42:02 by cbignon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,15 +49,15 @@ static int	ft_word_len(char const *s, char c)
 	return (w_len);
 }
 
-static void	*free_tab(char **tab)
+static void	*free_tab(char **tab, int j)
 {
-	int	j;
+	int	x;
 
-	j = 0;
-	while (tab[j])
+	x = 0;
+	while (x < j)
 	{
-		free(tab[j]);
-		j++;
+		free(tab[x]);
+		x++;
 	}
 	free(tab);
 	tab = NULL;
@@ -83,7 +83,7 @@ char		**ft_split(char const *s, char c)
 		k = 0;
 		if (!(tab[j] = (char*)malloc(sizeof(char)
 		* (ft_word_len(&s[i], c) + 1))))
-			return (free_tab(&tab[j]));
+			return (free_tab(tab, j));
 		while (s[i] != c && s[i] != '\0')
 			tab[j][k++] = s[i++];
 		tab[j][k] = '\0';
