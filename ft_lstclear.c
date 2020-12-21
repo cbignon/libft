892a7288217a5fca3e2e4a63c36614332b866f8c
cbignon <71795678+cbignon@users.noreply.github.com>
2020-12-21 12:25:13 +0100

    clear et delone modifagain

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: cbignon <cbignon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/18 11:46:48 by cbignon           #+#    #+#             */
-/*   Updated: 2020/12/21 12:16:29 by cbignon          ###   ########.fr       */
+/*   Updated: 2020/12/21 12:24:29 by cbignon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,8 @@ void	ft_lstclear(t_list **lst, void (*del)(void*))
 		while (*lst)
 		{
 			ptr = (*lst)->next;
-			del(*lst);
+			ft_lstdelone(*lst, del);
 			(*lst) = ptr;
 		}
-		lst = NULL;
 	}
 }
